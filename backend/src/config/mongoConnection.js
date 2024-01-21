304cb3ca5c5bd958a3287a095c032842c7e5278c
LelonDelonMelon <burkinggs@gmail.com>
2024-01-21 20:20:15 +0300

    vitalize

--- backend/src/config/mongoConnection.js
+++ backend/src/config/mongoConnection.js
@@ -1,9 +1,12 @@
-const mongo = require('mongoose');
-const Grid = require ('gridfs-stream');
+const mongo = require("mongoose");
+const Grid = require("gridfs-stream");
 
-module.exports = async() => {
-    await mongo.connect(process.env.MONGO_CONNECT_URL, ()=> {
-        console.log('connected to the mongodb cluster');
-
-    }, e=> console.log("Error", e))
-}
+module.exports = async () => {
+  await mongo.connect(
+    process.env.MONGO_CONNECT_URL,
+    () => {
+      console.log("connected to the mongodb cluster");
+    },
+    (e) => console.log("Error", e)
+  );
+};
